2816c0bf73fe41395f2d567ecdf9456eb28b4c82
iliarocks <me@ilia.page>
2026-08-09 22:35:27 -0700

    Organize Neovim configuration

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,14 +1,21 @@
-vim.opt.number = true
-vim.opt.relativenumber = true
+-- Editing
+
 vim.opt.expandtab = true
 vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.softtabstop = 2
-vim.opt.signcolumn = "yes"
-vim.opt.cursorline = true
 vim.opt.scrolloff = 4
 
+-- Appearance
+
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.signcolumn = "yes"
+vim.opt.cursorline = true
+
 vim.cmd.colorscheme("habamax")
+
+-- Diagnostics
 
 vim.diagnostic.config({
   underline = true,
@@ -20,24 +27,30 @@ vim.diagnostic.config({
   },
 })
 
+-- Packages
+
 vim.g["conjure#mapping#doc_word"] = "K"
 
 vim.pack.add({
   "https://github.com/Olical/conjure",
 })
 
+-- Language Servers
+
+local function show_lsp_hover()
+  vim.lsp.buf.hover({
+    max_width = 80,
+    max_height = 20,
+  })
+end
+
 vim.lsp.config("*", {
   on_attach = function(_, buffer)
-    vim.keymap.set("n", "K", function()
-      vim.lsp.buf.hover({
-        max_width = 80,
-        max_height = 20,
-      })
-    end, {
-    buffer = buffer,
-    desc = "LSP hover",
-  })
-end,
+    vim.keymap.set("n", "K", show_lsp_hover, {
+      buffer = buffer,
+      desc = "LSP hover",
+    })
+  end,
 })
 
 vim.lsp.config("clojure_lsp", {
